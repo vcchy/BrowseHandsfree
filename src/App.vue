e350d6cbcff400b5fbacffd30ba008450523f5a5
Oz Ramos <labofoz@gmail.com>
2018-04-29 17:11:33 -0700

    Adds a collapsable "toolbar panel" and updates webpack growl logo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template lang="pug">
   v-app
-    v-navigation-drawer(persistent v-model='drawer' enable-resize-watcher fixed app)
-      v-list
-        v-list-tile(value='true' v-for='(menuItem, i) in menuItems' :key='i')
-          v-list-tile-action
-            v-icon(v-html='menuItem.icon')
-          v-list-tile-content
-            v-list-tile-title(v-text='item.title')
-    v-toolbar(app)
-      v-toolbar-side-icon(@click.stop='drawer = !drawer')
-      v-toolbar-title(v-text='title')
+    v-toolbar(app dense color='yellow' flat extended style='position: relative')
+      v-toolbar-title
+        img.mr-2(src='@/assets/browsehandsfree-logo.png' height=30)
+        span.mr-2 BrowseHandsfree
+        small v{{$version}}
       v-spacer
+    v-layout(row pb-2)
+      v-flex.relative(xs8 offset-xs2 md6 offset-md3)
+        v-card.card--flex-toolbar
+          v-toolbar(card prominent dense dark color='purple lighten-2')
+            v-toolbar-title Start Here
+            v-spacer
+            v-btn(icon @click.stop='isMainPanelVisible = !isMainPanelVisible')
+              v-icon menu
+          v-divider
+          v-card-text(v-if='isMainPanelVisible').text-xs-center
+            p
+              img(src='@/assets/browsehandsfree-logo.png' alt='BrowseHandsfree' height=100)
+            p.text-xs-left Welcome to <b>BrowseHandsfree</b>, a platform that lets you browse the web hands-free (via face tracking)! To get started, lets first turn on your webcam.
+            p
+              v-btn(@click.stop='startWebcam' color='primary')
+                v-icon.mr-2 videocam
+                | Start Webcam
+            p
+              small - or -
+            p
+              router-link(to='about') Learn more
+
     v-content
       router-view
     v-footer(app)
@@ -19,21 +36,44 @@
 
 <script>
   export default {
+    name: 'App',
+
     data () {
       return {
-        clipped: false,
-        drawer: true,
-        fixed: false,
-        items: [{
+        isMainPanelVisible: true,
+        menuItems: [{
           icon: 'bubble_chart',
           title: 'Inspire'
-        }],
-        miniVariant: false,
-        right: true,
-        rightDrawer: false,
-        title: 'BrowseHandsfree'
+        }]
       }
     },
-    name: 'App'
+
+    methods: {
+      startWebcam () {
+        console.log('starting webcam')
+      }
+    }
   }
 </script>
+
+<style lang="stylus">
+  img
+    max-width: 100%
+
+  .toolbar__title
+    img
+      vertical-align: middle
+    small
+      font-size: 0.5em
+
+  .card--flex-toolbar
+    position: absolute
+    top: -48px
+    width: 100%
+
+  .toolbar--fixed.toolbar--extended
+    z-index: 0
+
+  .relative
+    position: relative
+</style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Package from '~/package.json'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 import {
   Vuetify,
   VApp,
-  VNavigationDrawer,
-  VFooter,
-  VList,
   VBtn,
-  VIcon,
+  VCard,
+  VDivider,
+  VFooter,
   VGrid,
+  VIcon,
+  VList,
   VToolbar,
+  VNavigationDrawer,
   transitions
 } from 'vuetify'
 import '../node_modules/vuetify/src/stylus/app.styl'
@@ -20,18 +23,25 @@ import '../node_modules/vuetify/src/stylus/app.styl'
 Vue.use(Vuetify, {
   components: {
     VApp,
-    VNavigationDrawer,
-    VFooter,
-    VList,
     VBtn,
-    VIcon,
+    VCard,
+    VDivider,
+    VFooter,
     VGrid,
+    VIcon,
+    VList,
+    VNavigationDrawer,
     VToolbar,
     transitions
+  },
+
+  theme: {
+    primary: 0xba68c8
   }
 })
 
 Vue.config.productionTip = false
+Vue.prototype.$version = Package.version
 
 /* eslint-disable no-new */
 new Vue({
